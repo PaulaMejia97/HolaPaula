4671629feddbf85f0dd10825554bcfbc8a0666e2
Paula Pardo <paulapardo9706@gmail.com>
2023-11-15 11:30:30 -0500

    Imprimir mensaje y numero

---
+++ src/Mensaje.java
@@ -0,0 +1,14 @@
+public class Mensaje{
+
+    public static void main(String[] args){
+
+        String nombre ="Paula";
+        System.out.println("Hola "+nombre);
+        int numero =5;
+        System.out.println(numero);
+
+
+
+
+    }
+}
